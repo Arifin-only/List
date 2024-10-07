08e4a071d8f91ea2994f03f3823627b6a5835f43
Arifin-only <arifin180104@gmail.com>
2024-10-07 20:20:46 +0800

    Create Deklarasi_Langsung_List.dart

---
+++ Deklarasi_Langsung_List.dart
@@ -0,0 +1,13 @@
+ void main(){
+   var namaOrang = ['Arifin', 'Upin', 'Ipin'];
+   var umurOrang = <int>[18, 28, 19];
+   var nilaiOrang = [
+     90,
+     80,
+     87
+   ];
+
+   print(namaOrang);
+   print(umurOrang);
+   print(nilaiOrang);
+ }
